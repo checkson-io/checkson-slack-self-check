0554251751e8f7b1284bbbda8a80933baa6bb173
Stefan Hudelmaier <hudelmaier@gmail.com>
2022-04-10 13:36:31 +0200

    Self check for webhooks

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import sys
-import random
+import os
+import requests
 
-if random.random() > 0.5:
-    print("Check successful")
+URL = os.environ.get('FLAPPING_STATE_URL')
+
+r = requests.get(URL)
+r.raise_for_status()
+
+state = r.json()['state']
+
+if state:
+    print("State is true, check successful")
     sys.exit(0)
 else:
-    print("Check unsuccessful")
+    print("State is false, check unsuccessful")
     sys.exit(1)

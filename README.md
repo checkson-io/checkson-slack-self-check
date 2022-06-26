fd3f171e86fc01ed26589ff05a0c9c6c1c994b7e
Stefan Hudelmaier <hudelmaier@gmail.com>
2022-06-26 10:24:42 +0200

    Change to slack check

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
-# checkson-webhook-self-check
+# checkson-slack-self-check
 
-This repository creates a Docker image that makes an HTTP request to a URL defined by the
-environment variable `FLAPPING_STATE_URL`. This endpoint returns `{"state": true}` and 
-`{"state": false}` in an alternating manner. This means that the check also alternates
-between successful and unsuccessful runs, so that on each run a webhook notification is 
-sent out.
+This repository creates a Docker image that retrieves the history of a Slack channel.
 
-An external metrics and alerting system (Influx Cloud) is used to detect if this notification
-has been sent out.
+It validates that Checkson sent notification via the Slack notification channel recently.
+The notification are triggered by this Checkson check: https://github.com/checkson-io/checkson-flapping-self-check
+
+The following environment variable must be defined.
--- app.py
+++ app.py
@@ -1,17 +1,37 @@
 import sys
+import logging
+from slack_sdk import WebClient
+import json
 import os
-import requests
 
-URL = os.environ.get('FLAPPING_STATE_URL')
+logging.basicConfig(level=logging.DEBUG)
 
-r = requests.get(URL)
-r.raise_for_status()
+SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')
+CHANNEL_ID = 'C03K8LKF0CT'
 
-state = r.json()['state']
-
-if state:
-    print("State is true, check successful")
-    sys.exit(0)
-else:
-    print("State is false, check unsuccessful")
+if SLACK_BOT_TOKEN is None:
+    print('Please define the env variable SLACK_BOT_TOKEN')
     sys.exit(1)
+
+
+def main():
+
+    client = WebClient(SLACK_BOT_TOKEN)
+    response = client.conversations_history(channel=CHANNEL_ID)
+
+    print(response)
+
+    for msg in response['messages']:
+        print(msg)
+
+    success = True
+    if success:
+        print("Recent notification detected, check successful")
+        sys.exit(0)
+    else:
+        print("There were no recent notifications, check unsuccessful")
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
